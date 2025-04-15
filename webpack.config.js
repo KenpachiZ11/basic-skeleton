35ca5baadf187f26437c315733a3db51363c047e
Быковский Даниил Викторович <bykovskydav@corp.lukoil.com>
2025-04-15 14:58:16 +0300

    настройка

---
+++ webpack.config.js
@@ -0,0 +1,100 @@
+// webpack.config.js
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const Dotenv = require('dotenv-webpack');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+module.exports = () => ({
+  mode: "development",
+  devtool: 'inline-source-map',
+  entry: path.resolve(__dirname, "./src/index.js"),
+  output: {
+    path: path.resolve(__dirname, "./build"),
+    filename: '[name].[contenthash].js',
+    clean: true,
+    publicPath: "/"
+  },
+  devServer: {
+    static: path.resolve(__dirname, "./public"),
+    port: 3000,
+    open: true,
+    hot: true,
+    historyApiFallback: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.svg$/,
+        use: ["@svgr/webpack", "url-loader"],
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        oneOf: [
+          {
+            resourceQuery: /^\?modules/,
+            use: [
+              MiniCssExtractPlugin.loader,
+              {
+                loader: 'css-loader',
+                options: {
+                  modules: {
+                    localIdentName: '[name]__[local]--[hash:base64:5]',
+                  },
+                  importLoaders: 1,
+                },
+              },
+              'sass-loader',
+            ]
+          },
+          {
+            use: [
+              MiniCssExtractPlugin.loader,
+              'css-loader',
+              'sass-loader',
+            ]
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpg|gif|JPG)$/,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|ttf|otf|eot)$/,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".json"]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "./public/index.html"),
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'styles.css',
+    }),
+    new Dotenv({
+      path: './.env',
+    })
+  ],
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        }
+      }
+    }
+  }
+});
